9aaa4d28088b65042b71a43a45e6b1204c091ce8
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2021-12-13 16:35:46 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var lwx = 0
 var lwy = 0
 var rwx = 0
 var rwy = 0
-
+var difference
 function setup(){
     video=createCapture(VIDEO)
     canvas=createCanvas(690,490)
@@ -14,7 +14,10 @@ function setup(){
 }
 
 function draw() {
-    background(0, 255, 200)
+    background(234, 0, 255)
+    textSize(difference)
+    fill(255, 166, 0)
+    text("Shruthi", 75, 350)
 }
 
 function modelLoaded() {
@@ -26,9 +29,14 @@ function gotPoses(results) {
         console.log(results);
         nx = results[0].pose.nose.x
         ny = results[0].pose.nose.y
+
         lwx = results[0].pose.leftWrist.x
         lwy = results[0].pose.leftWrist.y
         rwx = results[0].pose.rightWrist.x
         rwy = results[0].pose.rightWrist.y
+
+        difference = floor(lwx - rwx)
+
+        document.getElementById("fs").innerHTML= difference
     }
 }
